44d2f7f9fe6c469771386fa18cd59cec3c52cc1a
Dehal KRUSCHEV <dkrusche@e5r6p9.jnb.42.fr>
2016-11-03 17:07:58 +0200

    Bound parameters

--- models/validate_model.php
+++ models/validate_model.php
@@ -5,9 +5,13 @@ Class ValidateModel
 	public static function check($login, $hash)
 	{
 		$db = Db::getConnect()->getInstance();
-		$query = $db->prepare('SELECT id FROM users WHERE (username = :login OR email = :login) AND password = :hash AND protect = "1";');
-		$query->execute(array('login' => $login, 'hash' => $hash));
-		$ret = $query->fetch();
+		$protect = "1";
+		$query_match = $db->prepare('SELECT id FROM users WHERE (username = :login OR email = :login) AND password = :hash AND protect = :protect;');
+		$query_match->bindParam(":login", $login);
+		$query_match->bindParam(":hash", $hash);
+		$query_match->bindParam(":protect", $protect);
+		$query_match->execute();
+		$ret = $query_match->fetch();
 		if (empty($ret))
 			return 0;
 		else
